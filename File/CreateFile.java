ced87abe5d297fa14258c3d42f38204e23f6c2df
sahil <sk9711092314@gmail.com>
2022-08-07 12:40:27 +0530

    File is created on File folder

---
+++ File/CreateFile.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+class CreateFile{
+    public static void main(String[] args) {
+        try{
+
+            File newFile = new File("HelloFile.txt");
+            // File newFile2 = new File("HelloFile");
+            if(newFile.createNewFile()){
+                System.out.println("File is created"+ newFile.getName());
+            
+        }
+
+
+        }catch(IOException e){System.out.println(" An error occurred");
+            e.printStackTrace();
+    }
+        
+    }
+}
